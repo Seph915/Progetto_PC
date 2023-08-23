d12036fa286e79b9b3691bcc4e5835695f36bb61
Alex <alex97av@gmail.com>
2023-08-23 11:37:51 +0200

    some changes to storageList class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        StorageList storageList = new StorageList();
-        for (ObjStorage storage : storageList.getStorageList()) {
-                System.out.println(storage.storageDetails());
-            }
     }
 }
 
--- src/StorageList.java
+++ src/StorageList.java
@@ -2,18 +2,22 @@
 import java.util.ArrayList;
 
 public class StorageList {
-    private ArrayList<ObjStorage> storageList;
+    private ArrayList<ObjStorage> storageListArray;
+    ObjStorage samsung870Evo1Tb = new ObjStorage("Samsung", "870 Evo 1TB", "MZ-77E1T0BW", 1000, "SSD", "2.5", "SATA", false);
+    ObjStorage westernDigitalBlackSn7702Tb = new ObjStorage("Western Digital", "Black SN770 2TB", "WDS200T3X0E", 2000, "SSD", "M.2-2280", "M.2 PCIe 4.0 X4", true);
+    ObjStorage crucialMx5001Tb = new ObjStorage("Crucial", "MX500 1TB", "CT1000MX500SSD1", 1000, "SSD", "2.5", "SATA", false);
+    ObjStorage westernDigitalBlackSn850x1Tb = new ObjStorage("Western Digital", "Black SN850X 1TB", "WDS100T2X0E", 1000, "SSD", "M.2-2280", "M.2 PCIe 4.0 X4", true);
+    ObjStorage samsung990Pro1Tb = new ObjStorage("Samsung", "990 Pro 1TB", "MZ-V9P1T0BW", 1000, "SSD", "M.2-2280", "M.2 PCIe 4.0 X4", true);
 
     public StorageList() {
-        storageList = new ArrayList<>();
-        storageList.add(new ObjStorage("Samsung", "870 Evo 1TB", "MZ-77E1T0BW", 1000, "SSD", "2.5", "SATA", false));
-        storageList.add(new ObjStorage("Western Digital", "Black SN770 2TB", "WDS200T3X0E", 2000, "SSD", "M.2-2280", "M.2 PCIe 4.0 X4", true));
-        storageList.add(new ObjStorage("Crucial", "MX500 1TB", "CT1000MX500SSD1", 1000, "SSD", "2.5", "SATA", false));
-        storageList.add(new ObjStorage("Western Digital","Black SN850X 1TB","WDS100T2X0E",1000,"SSD","M.2-2280","M.2 PCIe 4.0 X4",true));
-        storageList.add(new ObjStorage("Samsung","990 Pro 1TB","MZ-V9P1T0BW",1000,"SSD","M.2-2280","M.2 PCIe 4.0 X4",true));
+        storageListArray.add(samsung870Evo1Tb);
+        storageListArray.add(westernDigitalBlackSn7702Tb);
+        storageListArray.add(westernDigitalBlackSn850x1Tb);
+        storageListArray.add(crucialMx5001Tb);
+        storageListArray.add(samsung990Pro1Tb);
     }
 
-    public ArrayList<ObjStorage> getStorageList() {
-        return storageList;
+    public ArrayList<ObjStorage> getStorageListArray() {
+        return storageListArray;
     }
 }
