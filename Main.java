5fa3d8ee4198966c72bbd5bcd8abc49886bb9df1
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-09-06 17:00:14 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,10 @@
 
-
 public class Main {
-    /*public static void main(String[] args) {
-        objCpu ryzen7 = new objCpu("Amd", "100-100000910WOF", 8 , 4.2 , 5
-        , "120 W ", "AMD Ryzen 7" ,"AM5" , "Readon ", 128);
-        printRyzen (ryzen7);
+    public static void main(String[] args) {
+        for (ObjCpu elements : ) {
 
-
-        objCpu i9 = new objCpu("Intel", " BX806849900K", 8, 3.6,5,
-                "95W" ,"Intel Core i9" , "LGA1151" , "intel UHD", 128);
-         printI9(i9);
+        }
+        System.out.println();
     }
-
-static void printRyzen(objCpu objCpu){
-System.out.println(objCpu.storageDetails() );
 }
-
-
-static void  printI9(objCpu objCpu){
-    System.out.println(objCpu.storageDetails());}*/
 }
